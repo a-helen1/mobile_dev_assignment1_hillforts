4828346d54d414e0139c38fb0926ad53010a3084
a-helen1 <andrewhelen1@gmail.com>
2020-11-01 20:44:46 +0000

    refactor org.wit.hillforts

--- app/src/main/java/org/wit/hillforts/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillforts/activities/HillfortActivity.kt
@@ -1,4 +1,4 @@
-package org.wit.hillforts
+package org.wit.hillforts.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,18 +7,21 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.hillforts.R
+import org.wit.hillforts.models.HillfortModel
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
+
+  var hillfort = HillfortModel()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_hillfort)
-    info("Placemark Activity started")
 
     btnAdd.setOnClickListener() {
-      val hillfortTitle = hillfortTitle.text.toString()
-      if (hillfortTitle.isNotEmpty()) {
-        info("add Button Pressed: $hillfortTitle")
+      hillfort.title = hillfortTitle.text.toString()
+      hillfort.description = hillfortDescription.text.toString()
+      if (hillfort.title.isNotEmpty()) {
+        info("add Button Pressed: $hillfort")
       } else {
         toast("Please enter a title")
       }
---
+++ app/src/main/java/org/wit/hillforts/models/HillfortModel.kt
@@ -0,0 +1,5 @@
+package org.wit.hillforts.models
+
+data class HillfortModel(
+            var title: String = "",
+            var description: String = "")
