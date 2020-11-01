a4f6e9d90eff7213ee6691d7d1f36e8f9ed33edf
a-helen1 <andrewhelen1@gmail.com>
2020-11-01 20:43:48 +0000

    add hillfort model, update layout to add description

--- app/src/main/java/org/wit/hillforts/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillforts/activities/HillfortActivity.kt
@@ -2,13 +2,26 @@ package org.wit.hillforts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hillfort.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
+import org.wit.hillforts.R
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_hillfort)
     info("Placemark Activity started")
+
+    btnAdd.setOnClickListener() {
+      val hillfortTitle = hillfortTitle.text.toString()
+      if (hillfortTitle.isNotEmpty()) {
+        info("add Button Pressed: $hillfortTitle")
+      } else {
+        toast("Please enter a title")
+      }
+    }
   }
 }
