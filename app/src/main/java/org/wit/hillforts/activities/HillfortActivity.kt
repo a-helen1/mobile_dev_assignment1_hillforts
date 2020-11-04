fc1b07d65edfd64d9496530ee464eb18c753b701
a-helen1 <andrewhelen1@gmail.com>
2020-11-04 22:00:30 +0000

    Add main app and hilforts array

--- app/src/main/java/org/wit/hillforts/activities/HillfortActivity.kt
+++ app/src/main/java/org/wit/hillforts/activities/HillfortActivity.kt
@@ -7,23 +7,30 @@ import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import org.wit.hillforts.R
+import org.wit.hillforts.main.MainApp
 import org.wit.hillforts.models.HillfortModel
 
 class HillfortActivity : AppCompatActivity(), AnkoLogger {
 
   var hillfort = HillfortModel()
+  lateinit var app : MainApp
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_hillfort)
+    app = application as MainApp
 
     btnAdd.setOnClickListener() {
       hillfort.title = hillfortTitle.text.toString()
       hillfort.description = hillfortDescription.text.toString()
       if (hillfort.title.isNotEmpty()) {
-        info("add Button Pressed: $hillfort")
+        app.hillforts.add(hillfort.copy())
+        info("add button pressed: ${hillfort}")
+        for (i in app.hillforts.indices) {
+          info("hillfort[$i]: ${app.hillforts[i]}")
+        }
       } else {
-        toast("Please enter a title")
+        toast ("Please enter a title")
       }
     }
   }
