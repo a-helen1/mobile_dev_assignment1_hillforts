db56c9a3e8d613a5aab4bf8c4ffba31a54e734ff
a-helen1 <andrewhelen1@gmail.com>
2020-11-08 20:50:15 +0000

    add user store and user JSON store

--- app/src/main/java/org/wit/hillforts/models/UserModel.kt
+++ app/src/main/java/org/wit/hillforts/models/UserModel.kt
@@ -4,7 +4,8 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class User(
+data class UserModel(
+    var id: Long = 0,
     var firstName: String = "",
     var lastName: String = "",
     var email: String = "",
