014a654b4cd098da9f71741db79559802e5d906d
a-helen1 <andrewhelen1@gmail.com>
2020-11-08 11:32:15 +0000

    add JSON persistance

--- app/src/main/java/org/wit/hillforts/main/MainApp.kt
+++ app/src/main/java/org/wit/hillforts/main/MainApp.kt
@@ -3,16 +3,19 @@ package org.wit.hillforts.main
 import android.app.Application
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.wit.hillforts.models.HillfortJSONStore
 import org.wit.hillforts.models.HillfortMemStore
 import org.wit.hillforts.models.HillfortModel
+import org.wit.hillforts.models.HillfortStore
 
 class MainApp : Application(), AnkoLogger {
 
-  val hillforts = HillfortMemStore()
+  lateinit var hillforts: HillfortStore
 
 
   override fun onCreate() {
     super.onCreate()
-    info("Placemark Started")
+    hillforts =HillfortJSONStore(applicationContext)
+    info("Hillfort Started")
   }
 }
